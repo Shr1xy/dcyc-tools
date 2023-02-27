8f8dbb649d925f82f92d5846860626c013f76488
Aleks <58r1x7@gmail.com>
2023-02-27 17:25:41 +0300

    bug fixes

--- src/utils/fetch-assets.js
+++ src/utils/fetch-assets.js
@@ -1,7 +1,5 @@
 export const fetchAssets = async (account) => {
   const url = 'https://api.waxsweden.org/v1/chain/get_table_rows'
-  //const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-  //const corsUrl = proxyUrl + url
 
   const allData = []
   const current_time = new Date()
@@ -59,7 +57,7 @@ export const fetchAssets = async (account) => {
       const hours = Math.floor(timeLeft / 1000 / 60 / 60)
 
       allCostGold.push(Number(parsedResponseMissions.rows[0]?.mission_cost[0]?.quantity.split(' ')[0] * 8).toFixed(3))
-      allCostWax.push(Number(parsedResponseMissions.rows[0]?.mission_cost[1]?.quantity.split(' ')[0] * 8).toFixed(3))
+      allCostWax.push(parsedResponseMissions.rows[0]?.mission_cost[1] ? Number(parsedResponseMissions.rows[0]?.mission_cost[1]?.quantity.split(' ')[0] * 8).toFixed(3) : Number(0).toFixed(3))
       allRewards.push(Number(parsedResponseMissions.rows[0]?.mission_reward_success[0]?.quantity.split(' ')[0] * 8).toFixed(3))
       pureProfit.push(Number(parsedResponseMissions.rows[0]?.mission_reward_success[0]?.quantity.split(' ')[0] * 8).toFixed(3) - Number(parsedResponseMissions.rows[0]?.mission_cost[0]?.quantity.split(' ')[0] * 8).toFixed(3))
 
@@ -68,26 +66,18 @@ export const fetchAssets = async (account) => {
         level: parsedResponseMissions.rows[0]?.allowlist[0][0]?.level_req,
         mission_name: parsedResponseMissions.rows[0]?.info[0]?.value,
         mission_cost_gold: Number(parsedResponseMissions.rows[0]?.mission_cost[0]?.quantity.split(' ')[0] * 8).toFixed(3),
-        mission_cost_wax: Number(parsedResponseMissions.rows[0]?.mission_cost[1]?.quantity.split(' ')[0] * 8).toFixed(3),
+        mission_cost_wax: parsedResponseMissions.rows[0]?.mission_cost[1] ? Number(parsedResponseMissions.rows[0]?.mission_cost[1]?.quantity.split(' ')[0] * 8).toFixed(3) : Number(0).toFixed(3),
         mission_reward: Number(parsedResponseMissions.rows[0]?.mission_reward_success[0]?.quantity.split(' ')[0] * 8).toFixed(3),
         missions_completed: item.completed_missions.length,
-        time_left: timeLeft > 0 ? `${hours >= 10 ? `${hours}` : hours < 10 && hours > 0 ? `0${hours}` : '00'}:${minutes >= 10 ? `${minutes}` : minutes < 10 && minutes > 0 ? `0${minutes}` : '00'}:${seconds >= 10 ? `${seconds}` : seconds < 10 && seconds > 0 ? `0${seconds}` : '00'}` : '00:00:00',
+        time_left:
+          timeLeft > 0
+            ? `${hours >= 10 ? `${hours}` : hours < 10 && hours > 0 ? `0${hours}` : '00'}:${minutes >= 10 ? `${minutes}` : minutes < 10 && minutes > 0 ? `0${minutes}` : '00'}:${seconds >= 10 ? `${seconds}` : seconds < 10 && seconds > 0 ? `0${seconds}` : '00'}`
+            : '00:00:00',
         available_at: item.available_at,
-        // allCostGold: allCostGold,
-        // allCostWax: allCostWax,
-        // allRewards: allRewards,
       }
       allData.push(obj)
     })
   )
-  // data.rows.forEach(async (item, index) => {
-
-  // })
-
-  //console.log(allData)
-  // allCostGold.reduce((a, b) => Number(a) + Number(b))
-  // allCostWax.reduce((a, b) => Number(a) + Number(b))
-  // allRewards.reduce((a, b) => Number(a) + Number(b))
 
   return {
     allData: allData,
